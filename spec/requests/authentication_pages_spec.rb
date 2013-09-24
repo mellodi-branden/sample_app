aee8648466a17f03f9a4c876dcf362ee2e37f623
Branden <branden@themellodi.com>
2013-09-24 17:45:42 -0500

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -45,6 +45,7 @@ describe "Authentication" do
       end
     end
 end
+
   describe "authorization" do
 
     describe "for non-signed-in users" do
@@ -57,6 +58,19 @@ end
           fill_in "Password", with: user.password
           click_button "Sign in"
         end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
 
         describe "after signing in" do
 
